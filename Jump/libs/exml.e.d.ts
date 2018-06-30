756bb4d64a2dad3e9851a6d002b6589fad4cfe61
Land <ludidilu@163.com>
2018-07-01 00:39:48 +0800

    a

--- Jump/libs/exml.e.d.ts
+++ Jump/libs/exml.e.d.ts
@@ -6,9 +6,21 @@ declare class AlertPanelSkin extends eui.Skin{
 }
 declare class MainPanelSkin extends eui.Skin{
 }
+declare class FenxiangButtonSkin extends eui.Skin{
+}
+declare class GenhuanjueseButtonSkin extends eui.Skin{
+}
+declare class GuanqiaButtonSkin extends eui.Skin{
+}
+declare class LeftButtonSkin extends eui.Skin{
+}
+declare class PaihangButtonSkin extends eui.Skin{
+}
 declare class RankCellSkin extends eui.Skin{
 }
 declare class RankPanelSkin extends eui.Skin{
 }
 declare class RightButtonSkin extends eui.Skin{
+}
+declare class WujinButtonSkin extends eui.Skin{
 }
